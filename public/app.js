d10dc3d6db88ddffb3620ec5880931f7749ed058
Yury Layeuski <ylayeuski@ylayeuski-mbp.local>
2016-06-09 20:48:14 -0700

    Add cart_get_number_of_items function

--- public/app.js
+++ public/app.js
@@ -17,9 +17,27 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	alert('Items in your cart' + cart_get_number_of_items()); //output numbers of items in cart
 }
 
 
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); //getting key from hash
+		var value = window.localStorage.getItem(key); //getting value from hash
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt=cnt+value*1;
+		}
+	}
+	return cnt;
+}
 
 
 
